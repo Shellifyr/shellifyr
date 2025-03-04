b491555223f9b6747e25e0e3536e7f516b666da0
Void <taboadajoao19122235@gmail.com>
2025-03-04 01:57:39 +0000

    fix(welcome/plugin): fixed plugin colors

--- plugins/welcome/welcome.sh
+++ plugins/welcome/welcome.sh
@@ -3,28 +3,80 @@
 # Creator: jappend
 # Shell: bash
 
-# Set and use colors, but only if the are supported by the terminal.
-number_colors_supported=
-if type -P tput &>/dev/null; then
-  number_colors_supported=$(tput colors 2>/dev/null || tput Co 2>/dev/null || echo -1)
-fi 
-
-RED=""
-GREEN=""
-YELLOW=""
-BLUE=""
-BOLD=""
-NORMAL=""
-
-if [[ -t 1 && $number_colors_supported && $number_colors_supported -ge 8 ]]; then
-  RED=$(tput setaf 1 2>/dev/null || tput AF 1 2>/dev/null)
-  GREEN=$(tput setaf 2 2>/dev/null || tput AF 2 2>/dev/null)
-  YELLOW=$(tput setaf 3 2>/dev/null || tput AF 3 2>/dev/null)
-  BLUE=$(tput setaf 4 2>/dev/null || tput AF 4 2>/dev/null)
-  BOLD=$(tput bold 2>/dev/null || tput md 2>/dev/null)
-  NORMAL=$(tput sgr0 2>/dev/null || tput me 2>/dev/null)
+# The [ -t 1 ] check only works when the function is not called from
+# a subshell (like in `$(...)` or `(...)`, so this hack redefines the
+# function at the top level to always return false when stdout is not
+# a tty.
+if [ -t 1 ]; then
+  is_tty() {
+    true
+  }
+else
+  is_tty() {
+    false
+  }
 fi
 
-printf '%s%s' "$NORMAL" "Welcome back, "
-printf '%s%s%s' "$BLUE" "$BOLD" "$(whoami)!"
-printf '%s\n' "$NORMAL"
+# Set and use colors, but only if the are supported by the terminal.
+function _supports_truecolor {
+  case "$COLORTERM" in
+  truecolor|24bit) return 0 ;;
+  esac
+
+  case "$TERM" in
+  iterm           |\
+  tmux-truecolor  |\
+  linux-truecolor |\
+  xterm-truecolor |\
+  screen-truecolor) return 0 ;;
+  esac
+
+  return 1
+}
+
+function _setup_color {
+  # Only use colors if connected to a terminal
+  if ! is_tty; then
+    FMT_RAINBOW=""
+    FMT_RED=""
+    FMT_GREEN=""
+    FMT_YELLOW=""
+    FMT_BLUE=""
+    FMT_BOLD=""
+    FMT_RESET=""
+    return
+  fi
+
+  if _supports_truecolor; then
+    FMT_RAINBOW="
+      $(printf '\033[38;2;255;0;0m')
+      $(printf '\033[38;2;255;97;0m')
+      $(printf '\033[38;2;247;255;0m')
+      $(printf '\033[38;2;0;255;30m')
+      $(printf '\033[38;2;77;0;255m')
+      $(printf '\033[38;2;168;0;255m')
+      $(printf '\033[38;2;245;0;172m')
+    "
+  else
+    FMT_RAINBOW="
+      $(printf '\033[38;5;196m')
+      $(printf '\033[38;5;202m')
+      $(printf '\033[38;5;226m')
+      $(printf '\033[38;5;082m')
+      $(printf '\033[38;5;021m')
+      $(printf '\033[38;5;093m')
+      $(printf '\033[38;5;163m')
+    "
+  fi
+
+  FMT_RED=$(printf '\033[31m')
+  FMT_GREEN=$(printf '\033[32m')
+  FMT_YELLOW=$(printf '\033[33m')
+  FMT_BLUE=$(printf '\033[34m')
+  FMT_BOLD=$(printf '\033[1m')
+  FMT_RESET=$(printf '\033[0m')
+}
+
+printf '%s%s' "$FMT_RESET" "Welcome back, "
+printf '%s%s%s' "$FMT_BLUE" "$FMT_BOLD" "$(whoami)!"
+printf '%s\n' "$FMT_RESET"
