41877a92d6b597ff9a58c16ab61d4480980bc4cb
Void <taboadajoao19122235@gmail.com>
2025-03-04 02:00:18 +0000

    refactor: changed shell color config

--- config/zsh_config.sh
+++ config/zsh_config.sh
@@ -7,36 +7,89 @@ PLUGINS_DIR="$SHELLIFYR_HOME/plugins"
 
 CURRENT_SHELL=$(basename "$SHELL")
 
-# Set and use colors, but only if the are supported by the terminal.
-number_colors_supported=
-if type -P tput &>/dev/null; then
-  number_colors_supported=$(tput colors 2>/dev/null || tput Co 2>/dev/null || echo -1)
-fi 
-
-RED=
-GREEN=
-YELLOW=
-BLUE=
-BOLD=
-NORMAL=
-if [[ -t 1 && $number_colors_supported && $number_colors_supported -ge 8 ]]; then
-  RED=$(tput setaf 1 2>/dev/null || tput AF 1 2>/dev/null)
-  GREEN=$(tput setaf 2 2>/dev/null || tput AF 2 2>/dev/null)
-  YELLOW=$(tput setaf 3 2>/dev/null || tput AF 3 2>/dev/null)
-  BLUE=$(tput setaf 4 2>/dev/null || tput AF 4 2>/dev/null)
-  BOLD=$(tput bold 2>/dev/null || tput md 2>/dev/null)
-  NORMAL=$(tput sgr0 2>/dev/null || tput me 2>/dev/null)
+# The [ -t 1 ] check only works when the function is not called from
+# a subshell (like in `$(...)` or `(...)`, so this hack redefines the
+# function at the top level to always return false when stdout is not
+# a tty.
+if [ -t 1 ]; then
+  is_tty() {
+    true
+  }
+else
+  is_tty() {
+    false
+  }
 fi
+
+# Set and use colors, but only if the are supported by the terminal.
+function _supports_truecolor {
+  case "$COLORTERM" in
+  truecolor|24bit) return 0 ;;
+  esac
+
+  case "$TERM" in
+  iterm           |\
+  tmux-truecolor  |\
+  linux-truecolor |\
+  xterm-truecolor |\
+  screen-truecolor) return 0 ;;
+  esac
+
+  return 1
+}
+
+function _setup_color {
+  # Only use colors if connected to a terminal
+  if ! is_tty; then
+    FMT_RAINBOW=""
+    FMT_RED=""
+    FMT_GREEN=""
+    FMT_YELLOW=""
+    FMT_BLUE=""
+    FMT_BOLD=""
+    FMT_RESET=""
+    return
+  fi
+
+  if _supports_truecolor; then
+    FMT_RAINBOW="
+      $(printf '\033[38;2;255;0;0m')
+      $(printf '\033[38;2;255;97;0m')
+      $(printf '\033[38;2;247;255;0m')
+      $(printf '\033[38;2;0;255;30m')
+      $(printf '\033[38;2;77;0;255m')
+      $(printf '\033[38;2;168;0;255m')
+      $(printf '\033[38;2;245;0;172m')
+    "
+  else
+    FMT_RAINBOW="
+      $(printf '\033[38;5;196m')
+      $(printf '\033[38;5;202m')
+      $(printf '\033[38;5;226m')
+      $(printf '\033[38;5;082m')
+      $(printf '\033[38;5;021m')
+      $(printf '\033[38;5;093m')
+      $(printf '\033[38;5;163m')
+    "
+  fi
+
+  FMT_RED=$(printf '\033[31m')
+  FMT_GREEN=$(printf '\033[32m')
+  FMT_YELLOW=$(printf '\033[33m')
+  FMT_BLUE=$(printf '\033[34m')
+  FMT_BOLD=$(printf '\033[1m')
+  FMT_RESET=$(printf '\033[0m')
+}
 
 # Checks if the config file exists
 if [[ -f "$CONFIG_FILE" ]]; then
   source "$CONFIG_FILE"
   if [[ $DEBUG_MODE == true ]]; then
-    printf '%s%s' "$BLUE" "DEBUG: sourced '$CONFIG_FILE'."
-    printf '%s\n' "$NORMAL"
+    printf '%s%s' "$FMT_BLUE" "DEBUG: sourced '$CONFIG_FILE'."
+    printf '%s\n' "$FMT_RESET"
   fi
 else
-  printf '%s%s%s' "$RED" "$BOLD" "FATAL: .shellifyrrc not found."
+  printf '%s%s%s' "$FMT_RED" "$FMT_BOLD" "FATAL: .shellifyrrc not found."
   exit 1
 fi 
 
@@ -50,20 +103,20 @@ for plugin in "${PLUGINS[@]}"; do
   if [[ -f "$PLUGIN_FILE" ]]; then
     if [[ -z "$COMPATIBILITY" || "$COMPATIBILITY" == *"$CURRENT_SHELL"* ]]; then 
       if [[ $DEBUG_MODE == true ]]; then
-        printf '%s%s' "$BLUE" "DEBUG: loading '$plugin' plugin..."
-        printf '%s\n' "$NORMAL"
+        printf '%s%s' "$FMT_BLUE" "DEBUG: loading '$plugin' plugin..."
+        printf '%s\n' "$FMT_RESET"
       fi
       source "$PLUGIN_FILE"
       if [[ $DEBUG_MODE == true ]]; then
-        printf '%s%s' "$BLUE" "DEBUG: loaded '$plugin' plugin."
-        printf '%s\n' "$NORMAL"
+        printf '%s%s' "$FMT_BLUE" "DEBUG: loaded '$plugin' plugin."
+        printf '%s\n' "$FMT_RESET"
       fi
     else 
-      printf '%s%s%s' "$YELLOW" "$BOLD" "Plugin '$plugin' not compatible with $CURRENT_SELL (only with: $COMPATIBILITY)."
-      printf '%s\n' "$NORMAL"
+      printf '%s%s%s' "$FMT_YELLOW" "$FMT_BOLD" "Plugin '$plugin' not compatible with $CURRENT_SELL (only with: $COMPATIBILITY)."
+      printf '%s\n' "$FMT_RESET"
     fi
   else
-    printf '%s%s%s' "$YELLOW" "$BOLD" "Plugin not found: $plugin"
-    printf '%s\n' "$NORMAL"
+    printf '%s%s%s' "$FMT_YELLOW" "$FMT_BOLD" "Plugin not found: $plugin"
+    printf '%s\n' "$FMT_RESET"
   fi 
 done
